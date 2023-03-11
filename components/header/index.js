9b5114f56635a5f5f05c1533cfd9679eedc8b420
Zury Martinez <zury.martinez.saldivia@gmail.com>
2023-03-11 15:11:30 -0300

    the header was created
    The header code was created, and it was styled with tailwind so that it is responsive for all types of devices.

---
+++ components/header/index.js
@@ -0,0 +1,95 @@
+import Link from "next/link";
+import { useState } from "react";
+
+function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header className="bg-gray-900 text-white">
+      <div className="mx-auto max-w-6xl px-4 py-4 md:flex md:items-center md:justify-between">
+        <div className="flex items-center justify-between">
+          <Link legacyBehavior href="/">
+            <a className="text-xl font-bold">Artificial Intelligence</a>
+          </Link>
+          <button
+            className="md:hidden"
+            onClick={toggleMenu}
+            aria-label="Menu Button"
+          >
+            {isMenuOpen ? (
+              <svg
+                className="w-6 h-6 fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M18.293 5.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-2 2a1 1 0 01-1.414-1.414L18.586 9H3a1 1 0 010-2h15.586l-1.293-1.293a1 1 0 010-1.414z" />
+              </svg>
+            ) : (
+              <svg
+                className="w-6 h-6 fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z" />
+              </svg>
+            )}
+          </button>
+        </div>
+        <nav
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } md:flex md:items-center`}
+        >
+          <ul className="text-lg md:flex-grow md:flex md:justify-end md:space-x-4">
+            <li>
+              <Link legacyBehavior href="/">
+                <a
+                  className="block px-2 py-1 rounded hover:bg-gray-700"
+                  onClick={toggleMenu}
+                >
+                  Home
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link legacyBehavior href="/history">
+                <a
+                  className="block px-2 py-1 rounded hover:bg-gray-700"
+                  onClick={toggleMenu}
+                >
+                  History
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link legacyBehavior href="/applications">
+                <a
+                  className="block px-2 py-1 rounded hover:bg-gray-700"
+                  onClick={toggleMenu}
+                >
+                  Applications
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link legacyBehavior href="/challenges">
+                <a
+                  className="block px-2 py-1 rounded hover:bg-gray-700"
+                  onClick={toggleMenu}
+                >
+                  Challenges
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [ "tailwindcss",
-    "postcss-flexbugs-fixes",
-    [
-      "postcss-preset-env",
-      {
-        autoprefixer: {
-          flexbox: "no-2009",
-        },
-        stage: 3,
-        features: {
-          "custom-properties": false,
-        },
-      },
-    ],
-  ],
+  plugins: [],
 }
