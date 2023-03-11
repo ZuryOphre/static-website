6e45c1a5db0b18a1a2ff127ee1e0fe2a88632723
Zury Martinez <zury.martinez.saldivia@gmail.com>
2023-03-11 14:06:44 -0300

    dependencies were installed
    The next application was created and the dependency of tailwind postcss and autoprefixer was installed to stylize the page without complexity

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [ "tailwindcss",
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        autoprefixer: {
+          flexbox: "no-2009",
+        },
+        stage: 3,
+        features: {
+          "custom-properties": false,
+        },
+      },
+    ],
+  ],
+}
